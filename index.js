55ff61acb292760621874a7fb17d3b64254e1ded
Joseph Wachira <washywachira@gmail.com>
2023-04-07 15:40:51 +0300

    completed individual project

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-# theProjectphase1
-Dog Breeds App
-This is a simple web application that displays random dog breeds with their images and allows users to leave comments about them. It was built using HTML, CSS, and JavaScript.
-
-Getting Started
-To run the app, simply open the index.html file in a web browser. The app uses the Dog API (https://dog.ceo/dog-api/) to fetch random dog breed images and a list of all dog breeds.
-
-Usage
-When the app is loaded, it displays a random dog breed image and name. Users can click the "Next" button to display a new random dog breed. The app also allows users to leave comments about the displayed dog breed. Users can enter their comments in the input field and click the "Submit" button to add their comment to the comment section.
-
-Files
-The app consists of three files:
-
-index.html: contains the HTML code for the app
-index.css: contains the CSS code for the app
-index.js: contains the JavaScript code for the app
-Acknowledgements
-This app was built using the Dog API (https://dog.ceo/dog-api/), which provides free access to a large database of dog images and breeds. Thanks to the creators of the Dog API for making this app possible.
-
-// Select the necessary elements from the HTML code
 const breedContainer = document.getElementById('breed-container');
 const breedName = document.getElementById('breed-name');
 const breedImage = document.getElementById('breed-image');
@@ -26,7 +6,6 @@ const commentContainer = document.getElementById('comment-container');
 const commentForm = document.getElementById('comment-form');
 const commentInput = document.getElementById('comment-input');
 
-// Initialize the current breed to null
 let currentBreed = null;
 
 // Function to fetch a random dog breed image
@@ -45,27 +24,21 @@ async function fetchAllBreeds() {
 
 // Function to display a new dog breed image and name
 async function displayNewBreed() {
-  // Initialize the new breed to the current breed
   let newBreed = currentBreed;
   // Keep fetching new breeds until we get one that is different from the current breed
   while (newBreed === currentBreed) {
     const breeds = await fetchAllBreeds();
     newBreed = breeds[Math.floor(Math.random() * breeds.length)];
   }
-  // Set the current breed to the new breed
   currentBreed = newBreed;
-  // Set the breed name and image to the new breed's name and image
   breedName.innerText = currentBreed;
   breedImage.src = await fetchBreedImage();
 }
 
 // Function to display a new comment
 function displayNewComment(comment) {
-  // Create a new paragraph element for the comment
   const commentElement = document.createElement('p');
-  // Set the text of the comment element to the comment text
   commentElement.innerText = comment;
-  // Add the comment element to the comment container
   commentContainer.appendChild(commentElement);
 }
 
@@ -74,13 +47,9 @@ nextButton.addEventListener('click', displayNewBreed);
 
 // Event listener for the comment form
 commentForm.addEventListener('submit', event => {
-  // Prevent the form from submitting normally
   event.preventDefault();
-  // Get the value of the comment input
   const comment = commentInput.value;
-  // Display the new comment
   displayNewComment(comment);
-  // Clear the comment input field
   commentInput.value = '';
 });
 
